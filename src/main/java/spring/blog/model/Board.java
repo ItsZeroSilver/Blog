ae6be47a87b89f033ca50226f0bc21b36847f718
ItsZeroSliver <ga417@naver.com>
2022-10-04 23:55:05 +0900

    reply, boad 테이블 수정 및 연관관계

--- src/main/java/spring/blog/model/Board.java
+++ src/main/java/spring/blog/model/Board.java
@@ -9,13 +9,14 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Boader {
+public class Board {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(nullable = false, length = 100)
@@ -28,6 +29,12 @@ public class Boader {
     @ManyToOne //Many = Board, User = One
     @JoinColumn(name = "userId")
     private User user;//DB는 오브젝트를 지정할 수 없지만, 자바는 오브젝트를 지정할 수 있다.
+
+    //DB 생성할 필요는 없음
+    //mappedBy 연관관계 주인이 아니다.(FK가 아님)
+    @OneToMany(mappedBy="board")
+    private List<Reply> reply;
+
     @CreationTimestamp
     private Timestamp createdDate;
 }
--- src/main/java/spring/blog/model/Reply.java
+++ src/main/java/spring/blog/model/Reply.java
@@ -21,8 +21,8 @@ public class Reply {
     @Column(nullable = false, length = 200)
     private String content;
     @ManyToOne
-    @JoinColumn(name="boaderId")
-    private Boader boader;
+    @JoinColumn(name="boardId")
+    private Board board;
     @ManyToOne
     @JoinColumn(name="userId")
     private User user;
